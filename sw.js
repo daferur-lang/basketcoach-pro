e1c01c46bf76af8de20be7b037d6b2d4c8f3e1b4
David Fernandez Urbano <daferur@gmail.com>
2026-05-16 11:11:39 +0200

    Initial commit: BasketCoach Pro PWA

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'basketcoach-v2';
+const SCOPE = new URL(self.registration.scope).pathname;
+const ASSETS = [SCOPE, SCOPE + 'index.html', SCOPE + 'manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match(SCOPE + 'index.html')))
+  );
+});
